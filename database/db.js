69aa5158433e40bde0e755588d36b3928a49c3f2
Sagar Chandore <83939401+Sagar9081@users.noreply.github.com>
2021-11-18 17:36:47 +0530

    Add files via upload

---
+++ database/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const Connection = async (username, password) => {
+  const URL = `mongodb+srv://${username}:${password}@cluster0.plsjd.mongodb.net/myBlog?retryWrites=true&w=majority`;
+  try {
+    await mongoose.connect(URL, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error while connecting to the database ", error);
+  }
+};
+
+export default Connection;
